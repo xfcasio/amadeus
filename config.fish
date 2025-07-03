0f9496ab330b287b4bf688136075718f1871dddd
xfcasio <xfc1@pm.me>
2025-07-03 13:33:24 +0300

    modify .cargo/config.toml + better history autocompletion for fish

--- config.fish
+++ config.fish
@@ -1,5 +1,8 @@
 # Fish configuration converted from zsh
 
+bind \e\[A history-prefix-search-backward
+bind \e\[B history-prefix-search-forward
+
 # Environment variables
 set -x EDITOR nvim
 set -x MANPAGER 'nvim +Man!'
